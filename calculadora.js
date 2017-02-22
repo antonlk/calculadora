06b16247a4a338c2d303afb8593059801a17bf0a
Anton <piscinadelamuertahlc@gmail.com>
2017-02-22 07:01:38 -0500

    version oficial 0.1 con js simplificado

--- calculadora.js
+++ calculadora.js
@@ -1,46 +1,34 @@
 // agregamos eventos de click a todos los botones
 
 $(function () {
-    $("#1").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("1").value;
+	
+    var res;
+    
+    $('[name="numeros"]').click(function(){
+    	document.getElementById("screen").value = $("#screen").val() + $(this).val();
+    	  if (res){
+        	$("#screen").val("");
+        	res="";
+        }
+       
     });
-    $("#2").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("2").value;
-    });
-    $("#3").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("3").value;
-    });
-    $("#4").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("4").value;
-    });
-    $("#5").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("5").value;
-    });
-    $("#6").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("6").value;
-    });
-    $("#7").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("7").value;
-    });
-    $("#8").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("8").value;
-    });
-    $("#9").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("9").value;
-    });
-    $("#0").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("0").value;
-    });
+
     $("#mas").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("mas").value;
+        document.getElementById("screen").value = $("#screen").val() +" "+$("#mas").val() +" ";
+
+      
     });
+
     $("#menos").click(function () {
-        document.getElementById("screen").value = document.getElementById("screen").value + document.getElementById("menos").value;
+        document.getElementById("screen").value = $("#screen").val() + " " +$("#menos").val()+" ";
     });
+
     $("#borrar").click(function () {
         document.getElementById("screen").value = "";
     });
+
     $("#igual").click(function () {
-        document.getElementById("screen").value = eval(document.getElementById("screen").value.toString());
+        res = eval(document.getElementById("screen").value.toString());
+        $("#screen").val(res);
     });
 });
