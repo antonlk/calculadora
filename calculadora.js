1b2feac2cf3cb7d95a42141a5d664c6334cd4629
Anton <piscinadelamuertahlc@gmail.com>
2017-03-01 04:15:57 -0500

    Ajustes del js

--- calculadora.js
+++ calculadora.js
@@ -2,28 +2,29 @@
 //esto es rama mult-div
 
 $(function () {
-	
     var res;
     
     $('[name="numeros"]').click(function(){
-    	document.getElementById("screen").value = $("#screen").val() + $(this).val();
     	  if (res){
         	$("#screen").val("");
         	res="";
         }
-       
+       document.getElementById("screen").value = $("#screen").val() + $(this).val();
     });
 
     $("#mas").click(function () {
         document.getElementById("screen").value = $("#screen").val() +" "+$("#mas").val() +" ";
-
-      
     });
 
     $("#menos").click(function () {
         document.getElementById("screen").value = $("#screen").val() + " " +$("#menos").val()+" ";
     });
-
+	$("#mult").click(function () {
+        document.getElementById("screen").value = $("#screen").val() + " " +$("#mult").val()+" ";
+    });
+	$("#div").click(function () {
+        document.getElementById("screen").value = $("#screen").val() + " " +$("#div").val()+" ";
+    });
     $("#borrar").click(function () {
         document.getElementById("screen").value = "";
     });
